f7cf11bdcfa4ade78da188cd06aa3750899c9754
idroidsdev <125539788+idroidsdev@users.noreply.github.com>
2023-02-21 19:47:41 +0530

    Revert "Update BoardConfig.mk"
    This reverts commit 1d546e671856192f9d112cd7dd95066533a99d14.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,12 @@
 #
-# Copyright 2019 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,14 +15,10 @@
 # limitations under the License.
 #
 
-# This contains the module build definitions for the hardware-specific
-# components for this device.
-#
-# As much as possible, those components should be built unconditionally,
-# with device-specific names to avoid collisions, to avoid device-specific
-# bitrot and build breakages. Building a component unconditionally does
-# *not* include it on all devices, so it is safe even with hardware-specific
-# components.
+DEVICE_PATH := device/MiTAC/N630
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm64
@@ -31,87 +28,57 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := msm8953
-TARGET_NO_BOOTLOADER := true
+# Assert
+TARGET_OTA_ASSERT_DEVICE := N630
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_COPY_OUT_VENDOR := vendor
 
 # Kernel
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7 androidboot.selinux=permissive
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 
-BOARD_KERNEL_CMDLINE += androidboot.console=ttyHSL0 
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom 
-BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 
-BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 
-BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci 
-BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78af000 
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image 
-BOARD_KERNEL_CMDLINE += loop.max_part=7 
-BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=false 
-BOARD_KERNEL_CMDLINE += buildvariant=user
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_TAGS_OFFSET := 0x0000100
 BOARD_RAMDISK_OFFSET := 0x01000000
-
-TARGET_PREBUILT_KERNEL := device/MiTAC/N630/prebuilt/Image.gz-dtb
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/MiTAC/N630
+TARGET_KERNEL_CONFIG := N630_defconfig
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8953
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
 
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0X04000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0X04000000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0XC0000000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0X05E2379E00
-BOARD_CACHEIMAGE_PARTITION_SIZE := 0X10000000
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 0X02000000
-BOARD_VENDORIMAGE_PARTITION_SIZE := 0X40000000
-# fs types
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_ROOT_EXTRA_SYMLINKS := /vendor/dsp:/dsp /vendor/firmware_mnt:/firmware
-
-# decryption
-TW_INCLUDE_CRYPTO := true
-TARGET_HW_DISK_ENCRYPTION := true
-TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+# Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
 
-# TWRP specific build flags
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-
-TW_USE_TOOLBOX := true
-TW_EXCLUDE_TWRPAPP := true
-TW_EXCLUDE_SUPERSU := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-TW_HAS_EDL_MODE := true
-TW_INCLUDE_NTFS_3G := true
-
-# disable mouse cursor
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-
-# landscape
-TW_THEME := landscape_hdpi
-TW_ROTATION := 90
-RECOVERY_TOUCHSCREEN_SWAP_XY := true
-RECOVERY_TOUCHSCREEN_FLIP_Y := true
-
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_DEFAULT_BRIGHTNESS := 200
-# TARGET_USE_CUSTOM_LUN_FILE_PATH causes compile-time error in TWRP for 9.0
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/gadget/lun0/file"
-
+TW_USE_TOOLBOX := true
