e17b147307eebfeb638ef61ab42187a327a09156
idroidsdev dev <koolkundan@gmail.com>
2023-09-02 06:33:44 +0000

    commit16

--- omni_N630.mk
+++ omni_N630.mk
@@ -1,8 +1,6 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) -- only for 64bit phones
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from device
-$(call inherit-product, device/ANDROID_DEVICE_SKY/device.mk) -- path to main device makefile
 
 # Inherit common product files.
 $(call inherit-product, vendor/pb/config/common.mk)
