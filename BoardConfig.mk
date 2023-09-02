879c41b11f4521abb5bdff6c5b5444e3953bd088
idroidsdev dev <koolkundan@gmail.com>
2023-09-02 09:38:49 +0000

    c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true            # To add info about F2FS Filesystem Data Block
 # Put The Size of your Recovery Partition below, get it from your "MT****_Android_scatter.txt"
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-BOARD_USES_FULL_RECOVERY_IMAGE := true      # Uncomment this line if you want to remove size restriction
+#BOARD_USES_FULL_RECOVERY_IMAGE := true      # Uncomment this line if you want to remove size restriction
 BOARD_FLASH_BLOCK_SIZE := 0                   # Might be different for your chip
 BOARD_HAS_NO_REAL_SDCARD := true              # Depricated
 # BOARD_HAS_NO_SELECT_BUTTON := true          # Depricated
